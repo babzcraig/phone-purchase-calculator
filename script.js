2094c11a42d19ccfb14e62a4d85003b9136183f5
Babasanya Craig <babscraig@gmail.com>
2016-07-01 22:05:29 +0100

    Initial Commit
    Wrote a few of the initial functions. Also set known variables and
    constants

---
+++ script.js
@@ -0,0 +1,39 @@
+const TAX_RATE = 0.10;
+const PHONE_PRICE = 19.99;
+const ACCESORY_PRICE = 8.99;
+const SPENDING_THRESHOLD = 30;
+
+var bankBalance = 129;
+var amount = 0;
+
+var formatAmount = function(amt) {
+    amt = amt.toFixed(2);
+    amt = String(amt);
+    amt = "$" + amt;
+    alert("Your total purchase price is " + amt)
+}
+
+var calculateTax = function(amt) {
+    amt = amt + (amt * TAX_RATE);
+    retrun amt;
+}
+
+
+
+for (var 1=0; i < bankBalance/PHONE_PRICE; i++) {
+    amount += PHONE_PRICE;
+    if (amount < bankBalance) {
+    amount = amount + ACCESORY_PRICE;
+    } else {
+    alert("You do not have enough money for this purchase")
+    }
+}
+
+
+calculateTax(amount);
+formatAmount(amount);
+
+
+
+
+
